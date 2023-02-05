70148d949a424c458421179da71c46113cea69e2
Chiamaka Ugwu <mannehills@gmail.com>
2023-02-05 12:20:34 +0100

    Update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./contact.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faPaperPlane, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faPaperPlane,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   return (
@@ -10,28 +14,27 @@ const Contact = () => {
         <div className="heading">
           <FontAwesomeIcon icon={faPaperPlane} className="h1 icon" bounce />
           <h1 className="h1">Contact Me</h1>
-          <h2 className="h2">Need a custom website/ frontend tutor? Get in touch</h2>
+          <h2 className="h2">
+            Need a custom website/ frontend tutor? Get in touch
+          </h2>
         </div>
         <div class="contact">
           <div class="box" data-aos="fade-up" data-aos-duration="800">
             <div class="icon">
-            <FontAwesomeIcon icon={faPhone} className="i" beatFade />
+              <FontAwesomeIcon icon={faPhone} className="i" beatFade />
             </div>
             <a href="tel:+2347066087325">(+234) 706-608-7325</a>
           </div>
           <div class="box" data-aos="fade-up" data-aos-duration="800">
             <div class="icon">
-            <FontAwesomeIcon icon={faEnvelope} className="i" beatFade />
+              <FontAwesomeIcon icon={faEnvelope} className="i" beatFade />
             </div>
             <a className="email" href="mailto:chiamakaugwu.m@gmail.com">
               chiamakaugwu.m@gmail.com
             </a>
           </div>
-          <form
-            method="POST"
-            name="contactForm"
-            data-netlify="true"
-          >
+          <form method="POST" name="contactForm">
+            <input type="hidden" name="form-name" value="contactForm" />
             <input type="text" name="fullname" placeholder="Full Name" />
             <input type="email" name="email" placeholder="Email Address" />
             <input type="text" name="subject" placeholder="Subject" />
@@ -42,7 +45,9 @@ const Contact = () => {
               rows="5"
               placeholder="Message"
             ></textarea>
-            <button type="submit" className="submit">Submit</button>
+            <button type="submit" className="submit">
+              Submit
+            </button>
           </form>
         </div>
       </div>
