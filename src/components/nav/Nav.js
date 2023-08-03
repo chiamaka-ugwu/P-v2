d1d5c55a97996cd871bc97f46749c60d629f8caa
Chiamaka Ugwu <mannehills@gmail.com>
2023-08-03 20:48:48 +0100

    Update

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -11,14 +11,14 @@ const Nav = () => {
 
   const onButtonClick = () => {
     // using Java Script method to get PDF file
-    fetch("Chiamaka.pdf").then((response) => {
+    fetch("ChiamakaUgwu.pdf").then((response) => {
       response.blob().then((blob) => {
         // Creating new object of PDF file
         const fileURL = window.URL.createObjectURL(blob);
         // Setting various property values
         let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = "Chiamaka.pdf";
+        alink.download = "ChiamakaUgwu.pdf";
         alink.click();
         toggle();
       });
