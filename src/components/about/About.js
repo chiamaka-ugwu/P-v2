3379b2026db360db6ac5ecfb52b4c811e4c06bdc
Chiamaka Ugwu <mannehills@gmail.com>
2025-03-19 19:26:36 +0100

    Updated projects

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./about.css";
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import myPhoto from '../../assets/images/myphoto.jpg';
+import myPhoto from "../../assets/images/myphoto.jpg";
 
 const About = () => {
   return (
     <>
       <div className="about" id="about">
         <div className="heading">
-          <FontAwesomeIcon icon={faAddressCard} className="h1 icon" beat  />
+          <FontAwesomeIcon icon={faAddressCard} className="h1 icon" beat />
           <h1 className="h1">About Me</h1>
         </div>
         <div className="about-content about1">
@@ -18,18 +18,28 @@ const About = () => {
           </div>
           <div className="right">
             <p>
-              I am a hardworking and success-driven front-end{" "}
-              <span className="pink">developer</span> who loves to build
-              beautiful and responsive websites for users to interact with. I am
-              very passionate about translating designs into codes while paying
-              rapt attention to accessiblity, thereby providing a friendly
-              interface that would to meet users' needs. <br /> <br /> I also
-              serve as a <span className="pink">tutor</span> for individuals who
-              desire to start up a career in Frontend Development. I have gained
-              valuable experience in the mentorship field and I enjoy imparting
-              frontend knowledge in willing individuals. I am constantly
-              learning new skills to help improve myself as a developer and
-              tutor.
+              Hi, I'm Chiamaka Ugwu, a{" "}
+              <span className="pink">frontend developer</span> passionate about
+              crafting seamless and dynamic web applications. With a strong
+              foundation in Javascript and React.js, I specialize in building
+              intuitive, high-performance user interfaces that enhance digital
+              experiences. I thrive on solving problems, optimizing performance,
+              and creating scalable solutions that bridge the gap between design
+              and functionality. Beyond writing clean and efficient code, I
+              enjoy collaborating with teams to bring ideas to life, ensuring
+              that every project is both user-friendly and technically sound. My
+              experience spans across building responsive web applications,
+              implementing modern authentication systems, and optimizing UI
+              performance for accessibility and speed.
+              <br /> <br />
+              In addition to developing web applications, I am also passionate
+              about knowledge sharing and mentorship. As a{" "}
+              <span className="pink">tutor</span>, I have guided aspiring
+              developers in mastering essential frontend technologies, helping
+              them build real-world projects and understand industry best
+              practices. My approach focuses on simplifying complex concepts,
+              fostering hands-on learning, and empowering individuals to grow
+              their technical skills with confidence.
             </p>
           </div>
         </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles/header.css";
 import heroImage from "../../assets/images/bg.jpg";
-import mobile from "../../assets/images/bg2.jpg"
+import mobile from "../../assets/images/bg2.jpg";
 import { Typewriter } from "react-simple-typewriter";
 import Button from "../button/Button";
 
@@ -18,13 +18,18 @@ const Header = () => {
           </h3>
           <h1>A FRONTEND DEVELOPER</h1>
           <p>
-            I'm a creative developer passionate about developing user friendly
-            and interactive frontend applications.
+            I am a creative frontend developer passionate about crafting
+            seamless user interfaces and optimizing digital experiences with
+            modern technologies. I specialise in building scalable and
+            interactive web solutions.
           </p>
           <h2 className="possess">
-            I possess strong skills in  
-            <span className="pink" style={{  marginLeft: "10px",fontWeight: "bold" }}>
-            {"<"}
+            Tools I work with include:
+            <span
+              className="pink"
+              style={{ marginLeft: "10px", fontWeight: "bold" }}
+            >
+              {"<"}
               <Typewriter
                 loop
                 cursor
@@ -32,14 +37,23 @@ const Header = () => {
                 typeSpeed={200}
                 deleteSpeed={50}
                 delaySpeed={1000}
-                words={["HTML", "CSS", "Javascript", "ReactJS", "NextJS", "TailwindCSS", "MUI", "Bootstrap"]}
+                words={[
+                  "HTML",
+                  "CSS",
+                  "Javascript",
+                  "ReactJS",
+                  "NextJS",
+                  "Tailwind CSS",
+                  "Material UI",
+                  "Bootstrap",
+                ]}
                 // onLoop={(loopCount) =>
                 //   console.log(`Just completed loop ${loopCount}`)
                 // }
               />
             </span>
           </h2>
-          <Button name='View Projects'/>
+          <Button name="Explore My Work" />
         </div>
       </div>
     </header>
